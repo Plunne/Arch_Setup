0275d94ba56e3b5026cd5699ad0ec5558071bde9
Plunne <plunne.celeste@gmail.com>
2021-06-23 02:05:44 +0200

    Update arch scripts

--- arch_install.sh
+++ arch_install.sh
@@ -15,17 +15,17 @@ mount /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi && mount -t vfat /dev/sda1 /mnt/boot/efi
 
 # Prepare CHROOT
-pacstrap /mnt base base-devel linux-lts linux-firmware grub efibootmgr sudo pacman-contrib git wget networkmanager dhcpcd
+pacstrap /mnt base base-devel linux-lts linux-firmware grub efibootmgr sudo pacman-contrib git wget networkmanager dhcpcd vim
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # START CHROOT
 echo "START CHROOT"
 arch-chroot /mnt <<EOF
 git clone https://github.com/Plunne/Arch_Setup.git
-cp -r Arch_Setup/arch-chroot.sh .
+cp -r Arch_Setup/arch_chroot.sh .
 EOF
 
-arch-chroot /mnt
+arch-chroot /mnt ./arch_chroot.sh
 
 echo "Unmounting..."
 umount -R /mnt
